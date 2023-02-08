bf4216cbc9928ea29c5fc37ebf0559affba31b21
Anshika <anshikanegi.cs19@gmail.com>
2023-02-08 14:05:07 +0530

    update

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+import { IconButton } from "@mui/material";
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>I am header</h2>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
