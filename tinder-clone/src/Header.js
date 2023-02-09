fcaf76085c8da4941ec258f5958f7d922eae13d8
Anshika <anshikanegi.cs19@gmail.com>
2023-02-09 13:06:30 +0530

    update

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -2,14 +2,24 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from '@mui/icons-material/Person';
 import { IconButton } from "@mui/material";
+import ForumIcon from '@mui/icons-material/Forum'
 
 function Header() {
     return (
         <div className="header">
-            <h2>I am header</h2>
             <IconButton>
                 <PersonIcon fontSize="large" className="header__icon"/>
             </IconButton>
+            <img
+            className="header__logo"
+            src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
+            alt=""
+            />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
+
+            
         </div>
     )
 }
